26a8bd22216ca1283d052c12ea5294c054c9e636
santipu <santipujolsau@gmail.com>
2022-12-04 13:16:17 +0100

    created constants folder to contain address of deployed NFT collection

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xf9f5DC5daa6D4126dDD375CE524fA7d70d773851"
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS }
