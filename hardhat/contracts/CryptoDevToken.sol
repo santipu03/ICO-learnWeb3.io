bffaef423505e5ac5f0a51ba067f423bc2174774
santipu <santipujolsau@gmail.com>
2022-12-04 13:12:11 +0100

    created token contract

--- hardhat/contracts/CryptoDevToken.sol
+++ hardhat/contracts/CryptoDevToken.sol
@@ -4,3 +4,78 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ICryptoDevs.sol";
+
+contract CryptoDevToken is Ownable, ERC20 {
+    ICryptoDevs CryptoDevsNFT;
+    uint256 public constant tokenPrice = 0.001 ether;
+    uint256 public constant tokensPerNFT = 10 * 10 ** 18;
+    uint256 public constant maxTotalSupply = 10000 * 10 ** 18;
+    mapping(uint256 => bool) public tokenIdsClaimed;
+
+    constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
+        CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);
+    }
+
+    /**
+     * @dev Mints `amount` number of CryptoDevTokens
+     * Requirements:
+     * - `msg.value` should be equal or greater than the tokenPrice * amount
+     */
+    function mint(uint256 _amount) public payable {
+        uint256 _requiredAmount = _amount * tokenPrice;
+        require(msg.value >= _requiredAmount, "Not enough Ether sent");
+        uint256 amountWithDecimals = _amount * 10 ** 18;
+        // total tokens + amount <= 10000, otherwise revert the transaction
+        require(
+            (totalSupply() + amountWithDecimals) <= maxTotalSupply,
+            "Exceeds the max total supply available"
+        );
+        _mint(msg.sender, amountWithDecimals);
+    }
+
+    /**
+     * @dev Mints tokens based on the number of NFT's held by the sender
+     * Requirements:
+     * balance of Crypto Dev NFT's owned by the sender should be greater than 0
+     * Tokens should have not been claimed for all the NFTs owned by the sender
+     */
+    function claim() public {
+        address sender = msg.sender;
+        uint256 balance = CryptoDevsNFT.balanceOf(sender);
+        require(balance > 0, "You dont own any Crypto Dev NFT's");
+
+        // amount keeps track of number of unclaimed tokenIds
+        uint256 amount = 0;
+        // loop over the balance and get the token ID owned by `sender` at a given `index` of its token list.
+        for (uint256 i = 0; i < balance; i++) {
+            uint256 tokenId = CryptoDevsNFT.tokenOfOwnerByIndex(sender, i);
+            // if the tokenId has not been claimed, increase the amount
+            if (!tokenIdsClaimed[tokenId]) {
+                amount += 1;
+                tokenIdsClaimed[tokenId] = true;
+            }
+        }
+        // If all the token Ids have been claimed, revert the transaction;
+        require(amount > 0, "You have already claimed all the tokens");
+        // Mint (amount * 10) tokens for each NFT
+        _mint(msg.sender, amount * tokensPerNFT);
+    }
+
+    /**
+     * @dev withdraws all ETH sent to this contract
+     * Requirements:
+     * wallet connected must be owner's address
+     */
+    function withdraw() public onlyOwner {
+        uint256 amount = address(this).balance;
+        require(amount > 0, "Nothing to withdraw, contract balance emtpy");
+
+        address _owner = owner();
+        (bool sent, ) = _owner.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+}
